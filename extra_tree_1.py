a2e74f087cc7b0ce6430e28ff1b6db0c284edc04
tubadc <tubadc@bitbucket.org>
2016-04-07 23:48:45 -0400

    es

--- extra_tree_1.py
+++ extra_tree_1.py
@@ -1,14 +1,15 @@
 # Based on : https://www.kaggle.com/chabir/bnp-paribas-cardif-claims-management/extratreesclassifier-score-0-45-v5/code
 import pandas as pd
-
+import time
 import numpy as np
 import csv
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import ExtraTreesClassifier, VotingClassifier, RandomForestClassifier
 from sklearn import ensemble
 from sklearn.metrics import log_loss, make_scorer
 from sklearn import cross_validation
 from sklearn import preprocessing
+import xgboost as xgb
 
 def find_denominator(df, col):
     """
@@ -126,24 +127,83 @@ test = test.drop(['target', 'ID'], axis=1)
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(train, target, random_state=1301, test_size=0.3)
 
 print('Training...')
-extc = ExtraTreesClassifier(n_estimators=1000,
+etc1 = ExtraTreesClassifier(n_estimators=1000,
                             max_features=50,
                             criterion='entropy',
                             min_samples_split=4,
                             max_depth=35,
                             min_samples_leaf=2,
                             n_jobs=-1,
+                            random_state=2017,
                             verbose=2)
-# score = log_loss(y_test, extc.predict_proba(X_test)[:, 1])
 
-# scores = cross_validation.cross_val_score(extc, X_train, y_train, scoring='log_loss', cv=5, verbose=2)
-# print(scores.mean())
+etc2 = ExtraTreesClassifier(n_estimators=1000,
+                            max_features=50,
+                            criterion='gini',
+                            min_samples_split=4,
+                            max_depth=35,
+                            min_samples_leaf=2,
+                            n_jobs=-1,
+                            random_state=9527,
+                            verbose=2)
+
+etc3 = ExtraTreesClassifier(n_estimators=100,
+                            max_features=50,
+                            criterion='entropy',
+                            min_samples_split=4,
+                            max_depth=35,
+                            min_samples_leaf=2,
+                            n_jobs=-1,
+                            random_state=9527,
+                            warm_start=True,
+                            verbose=2)
+
+rf1 = RandomForestClassifier(bootstrap=True,
+                                 criterion='entropy',
+                                 min_samples_split=4,
+                                 min_samples_leaf=2,
+                                 max_features=50,
+                                 max_depth=35,
+                                 n_estimators=1000,
+                                 n_jobs=4,
+                                 oob_score=False,
+                                 random_state=1301,
+                                 verbose=2)
+
+xgb1 = xgb.XGBClassifier(max_depth=11,
+                            n_estimators=1000,
+                            learning_rate=0.05,
+                            subsample=0.96,
+                            colsample_bytree=0.40,
+                            colsample_bylevel=0.40,
+                            objective='binary:logistic',
+                            nthread=4,
+                            seed=2017)
+
+xgb2 = xgb.XGBClassifier(max_depth=11,
+                            n_estimators=100,
+                            learning_rate=0.03,
+                            subsample=0.96,
+                            colsample_bytree=0.45,
+                            colsample_bylevel=0.45,
+                            objective='binary:logistic',
+                            nthread=4,
+                            seed=1313)
+#score = log_loss(y_test, extc.predict_proba(X_test)[:, 1])
+
+clfs = [('etc', etc1), ('rf', rf1), ('xgb', xgb1), ('etc2', etc2)]
+# # set up ensemble of rf_1 and rf_2
+clf = VotingClassifier(estimators=clfs, voting='soft', weights=[1, 1, 1, 1])
+# st = time.time()
+# scores = cross_validation.cross_val_score(clf, X_train, y_train, scoring='log_loss', cv=2, verbose=2)
+# print(scores.mean()*-1)
+# print("time elaspe", time.time() - st)
 # exit()
 
-extc.fit(train, target)
+clf.fit(train, target)
 print('Predict...')
-y_pred = extc.predict_proba(test)
+y_pred = clf.predict_proba(test)
 
 # print y_pred
 
-pd.DataFrame({"ID": id_test, "PredictedProb": y_pred[:, 1]}).to_csv('data/extra_trees_045.csv', index=False)
+pd.DataFrame({"ID": id_test, "PredictedProb": y_pred[:, 1]}).to_csv('data/extra_trees_1_4.csv', index=False)
